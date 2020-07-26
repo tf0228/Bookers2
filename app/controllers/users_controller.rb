13d29ef21c4d289030b0a6d9371d4719a2010cab
vagrant <vagrant@localhost.localdomain>
2020-07-26 09:33:39 +0000

    [Update] user function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    @user = User.find(current_user.id)
   end
 
   def show
@@ -7,5 +9,17 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user), flash: {notice: "You have updated user successfully." }
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :profile_image, :introduction)
+    end
 end
